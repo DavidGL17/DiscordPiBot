ae67e30d8c888331853e0a878a4f1f183c0c2d87
DavidGL17 <david.gonzalez@citycable.ch>
2022-09-13 13:47:21 +0200

    bot working, message not done yet

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import time
 import json
 import os
 import discord
+from discord.ext import tasks
 
 # Settings
-DEFAULT_WAIT_TIME = 600  # 10 minutes
+DEFAULT_WAIT_TIME = 5  # 10 minutes
 TOKEN_FILE_NAME = "token.txt"
+CONTROL_FILE = "control.json"
 MESSAGE_TITLE = "New Raspberry Pi found!"
 
 # Feed URL
-FEED_URL = "https://rpilocator.com/feed/"
+FEED_URL = "https://rpilocator.com/feed/?country=CH&cat=CM4,PI4,PIZERO"
 
 # User Agent
 USER_AGENT = "DiscordPi feed alert"
@@ -19,51 +21,17 @@ CHANNEL_ID = 1019198548149022720
 
 # Create the message body
 def formatMessage(entry):
-    message = {
-        "title": MESSAGE_TITLE,
-        "message": entry.title + ": " + entry.link,
-        "extras": {"client::notification": {"click": {"url": entry.link}}},
-    }
-
-    message = json.dumps(message)
+    message = f"Test with the entries : {entry}, {entry.id}, {entry.title}, {entry.link}, {entry.published}"
 
     return message
 
 
-# Main program
-
-# Activate the discord client
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
-
-
-# read the token from the file
-TOKEN = open(TOKEN_FILE_NAME, "r").read()
-
-client.run(TOKEN)
-
-
-# Set control to blank list
-control = []
-
-# Fetch the feed
-f = feedparser.parse(FEED_URL, agent=USER_AGENT)
-
-# If there are entries in the feed, add entry guid to the control variable
-if f.entries:
-    for entries in f.entries:
-        control.append(entries.id)
-
-# Only wait 30 seconds after initial run.
-time.sleep(30)
-
-client.se
-
-while True:
+@tasks.loop(seconds=DEFAULT_WAIT_TIME)
+async def feedWatcher():
+    # Read the control list
+    with open(CONTROL_FILE, "r") as controlFile:
+        control = json.load(controlFile)
+    print("Checking feed...")
     # Fetch the feed again, and again, and again...
     f = feedparser.parse(FEED_URL, agent=USER_AGENT)
 
@@ -76,13 +44,55 @@ while True:
 
             message = formatMessage(entries)
 
-            sendMessage(message)
+            await client.get_channel(CHANNEL_ID).send(message)
 
             # Add entry guid to the control variable
             newControl.append(entries.id)
+        else:
             control.remove(entries.id)
 
-    # Replace old control with new one to update the currently available entries
-    control = newControl
+    # TODO add a logging of items that are not in the feed anymore
 
-    time.sleep(DEFAULT_WAIT_TIME)
+    # Write the new control list to the control file
+    with open(CONTROL_FILE, "w") as controlFile:
+        json.dump(newControl, controlFile)
+
+
+# Main program
+
+# Activate the discord client
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+    # Start the feed watcher
+    feedWatcher.start()
+
+
+# Setup the control list if it does not exist
+if not os.path.isfile(CONTROL_FILE):
+    # Set control to blank list
+    control = []
+
+    # Fetch the feed
+    f = feedparser.parse(FEED_URL, agent=USER_AGENT)
+
+    # If there are entries in the feed, add entry guid to the control variable
+    if f.entries:
+        for entries in f.entries:
+            control.append(entries.id)
+
+    # Write the list to a json file for later use
+    with open(CONTROL_FILE, "w") as outfile:
+        json.dump(control, outfile)
+
+    # Only wait 30 seconds after initial run.
+    time.sleep(30)
+
+# read the token from the file
+TOKEN = open(TOKEN_FILE_NAME, "r").read()
+
+client.run(TOKEN)
