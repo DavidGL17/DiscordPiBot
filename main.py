85192578c196697781b7cb95322cb17e2f0723e9
DavidGL17 <david.gonzalez@citycable.ch>
2022-09-15 17:57:16 +0200

    added settings file

--- main.py
+++ main.py
@@ -4,20 +4,11 @@ import json
 import os
 import discord
 from discord.ext import tasks
+from settingsModule import Settings
 
-# Settings
-INITIAL_WAIT_TIME = 30  # 30 seconds
-DEFAULT_WAIT_TIME = 30  # 10 minutes
-TOKEN_FILE_NAME = "token.txt"
-CONTROL_FILE = "control.json"
-MESSAGE_TITLE = "New Raspberry Pi found!"
-
-# Feed URL
-FEED_URL = "https://rpilocator.com/feed/?country=DE&cat=CM4,PI4,PIZERO"
-
-# User Agent
-USER_AGENT = "DiscordPi feed alert"
-CHANNEL_ID = 1019198548149022720
+##
+## Setup functions
+##
 
 # Create the message body
 def formatMessage(entry, isNew=True):
@@ -32,26 +23,26 @@ def formatMessage(entry, isNew=True):
     return message
 
 
-@tasks.loop(seconds=DEFAULT_WAIT_TIME)
+@tasks.loop(seconds=settings.DEFAULT_WAIT_TIME)
 async def feedWatcher():
     # Read the control list
-    with open(CONTROL_FILE, "r") as controlFile:
+    with open(settings.CONTROL_FILE, "r") as controlFile:
         control = json.load(controlFile)
     # Fetch the feed again, and again, and again...
-    f = feedparser.parse(FEED_URL, agent=USER_AGENT)
+    f = feedparser.parse(settings.FEED_URL, agent=settings.USER_AGENT)
 
     # Compare feed entries to control list.
     # If there are new entries, send a message/push
     # and add the new entry to new control list.
     newControl = []
     newItems = 0
-    await client.get_channel(CHANNEL_ID).send("Checking feed...")
+    await client.get_channel(settings.CHANNEL_ID).send("Checking feed...")
     for entries in f.entries:
         newControl.append(entries.id)
         if entries.id not in control:
             message = formatMessage(entries)
 
-            await client.get_channel(CHANNEL_ID).send(message)
+            await client.get_channel(settings.CHANNEL_ID).send(message)
 
             # Add entry guid to the control variable
             newItems += 1
@@ -61,16 +52,9 @@ async def feedWatcher():
     # TODO add a logging of items that are not in the feed anymore
 
     # Write the new control list to the control file
-    with open(CONTROL_FILE, "w") as controlFile:
+    with open(settings.CONTROL_FILE, "w") as controlFile:
         json.dump(newControl, controlFile)
     print(f"Checking done! warned for {newItems} new items")
-
-
-# Main program
-
-# Activate the discord client
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
 
 
 @client.event
@@ -80,14 +64,25 @@ async def on_ready():
     feedWatcher.start()
 
 
+# Main program
+
+# Import settings and token from config files
+settings = Settings()
+TOKEN = open(settings.TOKEN_FILE_NAME, "r").read()
+
+# Activate the discord client
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
 # Setup the control list if it does not exist
-if not os.path.isfile(CONTROL_FILE):
+if not os.path.isfile(settings.CONTROL_FILE):
     print("Doing initial setup...")
     # Set control to blank list
     control = []
 
     # Fetch the feed
-    f = feedparser.parse(FEED_URL, agent=USER_AGENT)
+    f = feedparser.parse(settings.FEED_URL, agent=settings.USER_AGENT)
 
     # If there are entries in the feed, add entry guid to the control variable
     if f.entries:
@@ -95,13 +90,10 @@ if not os.path.isfile(CONTROL_FILE):
             control.append(entries.id)
 
     # Write the list to a json file for later use
-    with open(CONTROL_FILE, "w") as outfile:
+    with open(settings.CONTROL_FILE, "w") as outfile:
         json.dump(control, outfile)
 
     # Only wait 30 seconds after initial run.
-    time.sleep(INITIAL_WAIT_TIME)
-
-# read the token from the file
-TOKEN = open(TOKEN_FILE_NAME, "r").read()
+    time.sleep(settings.INITIAL_WAIT_TIME)
 
 client.run(TOKEN)
---
+++ settingsModule.py
@@ -0,0 +1,10 @@
+import yaml
+
+
+class Settings:
+    def __init__(self):
+        self._settings = self._load_settings()
+
+    def _load_settings(self):
+        with open("settings.yaml", "r") as f:
+            return yaml.safe_load(f)
